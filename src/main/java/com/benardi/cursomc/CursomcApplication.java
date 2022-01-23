db5f70358f50e41dfa560978c9d82b53f97c8913
Benardi Nunes <benardinunes@gmail.com>
2022-01-23 09:53:54 -0300

    Add db initialization on app start-up

--- src/main/java/com/benardi/cursomc/CursomcApplication.java
+++ src/main/java/com/benardi/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.benardi.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.benardi.cursomc.domain.Category;
+import com.benardi.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category ctg1 = new Category(null, "Informática");
+		Category ctg2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(ctg1, ctg2));
+
 	}
 
 }
