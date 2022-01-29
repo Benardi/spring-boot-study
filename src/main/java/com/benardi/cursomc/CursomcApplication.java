fa600a1dc9988360a7e09e59a238865670b5a815
Benardi Nunes <benardinunes@gmail.com>
2022-01-29 10:59:49 -0300

    Add state and city classes and repositories

--- src/main/java/com/benardi/cursomc/CursomcApplication.java
+++ src/main/java/com/benardi/cursomc/CursomcApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.benardi.cursomc.domain.Category;
+import com.benardi.cursomc.domain.City;
 import com.benardi.cursomc.domain.Product;
+import com.benardi.cursomc.domain.State;
 import com.benardi.cursomc.repositories.CategoryRepository;
+import com.benardi.cursomc.repositories.CityRepository;
 import com.benardi.cursomc.repositories.ProductRepository;
+import com.benardi.cursomc.repositories.StateRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private StateRepository stateRepository;
+	@Autowired
+	private CityRepository cityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -33,16 +41,30 @@ public class CursomcApplication implements CommandLineRunner {
 		Product p1 = new Product(null, "Computador", 2000.00);
 		Product p2 = new Product(null, "Impressora", 800.00);
 		Product p3 = new Product(null, "Mouse", 80.00);
-		
-		ctg1.getProducts().addAll(Arrays.asList(p1,p2,p3));
+
+		ctg1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		ctg2.getProducts().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategories().addAll(Arrays.asList(ctg1));
 		p2.getCategories().addAll(Arrays.asList(ctg1, ctg2));
 		p3.getCategories().addAll(Arrays.asList(ctg1));
 
 		categoryRepository.saveAll(Arrays.asList(ctg1, ctg2));
-		productRepository.saveAll(Arrays.asList(p1,p2,p3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		/* State and City */
+		State est1 = new State(null, "Minas Gerais");
+		State est2 = new State(null, "São Paulo");
+
+		City c1 = new City(null, "Uberlândia", est1);
+		City c2 = new City(null, "São Paulo", est2);
+		City c3 = new City(null, "Campinas", est2);
+
+		est1.getCities().addAll(Arrays.asList(c1));
+		est2.getCities().addAll(Arrays.asList(c2, c3));
+
+		stateRepository.saveAll(Arrays.asList(est1, est2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ src/main/java/com/benardi/cursomc/repositories/CityRepository.java
@@ -0,0 +1,11 @@
+package com.benardi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.benardi.cursomc.domain.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+}
