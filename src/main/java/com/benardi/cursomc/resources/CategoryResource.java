1909363ea9d6a15af764891adcf5a38e5ed289aa
Benardi Nunes <benardinunes@gmail.com>
2022-01-30 19:09:35 -0300

    Add POST endpoint for Category

--- src/main/java/com/benardi/cursomc/resources/CategoryResource.java
+++ src/main/java/com/benardi/cursomc/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package com.benardi.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.benardi.cursomc.domain.Category;
 import com.benardi.cursomc.services.CategoryService;
@@ -22,6 +26,13 @@ public class CategoryResource {
 		Category obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
